0bf2e02ee6fb9997cf2c3f58e9082f5ea0e2df6b
Jessica Manak <jessica@Jessicas-MacBook-Air.local>
2021-02-26 08:36:31 -0800

    Request2

--- index.js
+++ index.js
@@ -75,9 +75,14 @@ console.log(animalNames(zooAnimals));
   For example: ['jackal, asiatic', .....]
   */
 
-  function lowerCaseNames(/*Your Code Here*/){
-    /*Your Code Here*/
+  function lowerCaseNames(array){
+    const newArray = array.map(function(item){
+      return item.animal_name.toLowerCase();
+    });
+    return newArray;
   }
+
+  console.log(lowerCaseNames(zooAnimals));
   
   
   /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
